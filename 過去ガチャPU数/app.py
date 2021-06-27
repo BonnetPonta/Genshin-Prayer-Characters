782ad45bfe9424c9019c7938b48c606249eb5e40
BonnetPonta <80739002+BonnetPonta@users.noreply.github.com>
2021-06-27 14:53:27 +0900

    imort順番を変更

--- 過去ガチャPU数/app.py
+++ 過去ガチャPU数/app.py
@@ -1,7 +1,7 @@
 # https://h1g.jp/genshin/?過去のガチャ情報
-from pprint import pprint
 from collections import Counter
 from json import load
+from pprint import pprint
 
 # g は gatya　の略
 with open(r"DB.json", "r", encoding="utf-8_sig") as f:
